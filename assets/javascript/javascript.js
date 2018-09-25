b85140a7e044b9a4bdf42455bc38079b3522f5f5
Renee Wysocki <reneew311@gmail.com>
2018-09-25 17:54:02 -0500

    add new button

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,6 +1,6 @@
 $( document ).ready(function() {
 
-    var searchOptions = ["horse", "dog", "squirrel", "parrot", "duck", "pig", "fish", "whale"];
+    var searchOptions = ["horse", "dog", "squirrel", "parrot", "pig", "fish", "whale"];
     var gifRating = "";
     var gifURL = "";
 
@@ -12,6 +12,7 @@ $( document ).ready(function() {
         for (var i = 0; i < searchOptions.length; i++) {
           var a = $("<button>");
           a.addClass("search-button");
+          a.addClass("col");
           a.attr('id', searchOptions[i])
           a.attr("data-name", searchOptions[i]);
           a.text(searchOptions[i]);
@@ -34,18 +35,32 @@ $( document ).ready(function() {
             $("#gifs").empty();
             for (let j = 0; j < response.data.length; j++) {
                 gifURL = response.data[j].images.original.url
-                gifRating = response.data[j].rating
+                gifRating = response.data[j].rating.toUpperCase()
                 console.log(gifURL);
                 console.log(gifRating);
-                $("#gifs").append("<img src='" + gifURL + "'>")
+                $("#gifs").append("<div class='container'> <div><img class='img-fluid' src='" + gifURL + "'><p>Rating: " + gifRating + "</p></div></div>")
             }
         });
     };
+
+    function addNewButton(){
+        $("#addGif").on("click", function(){
+        var input = $("#user-input").val().trim();
+        if (input == ""){
+          return false; // added so user cannot add a blank button
+        }
+        searchOptions.push(input);
+    
+        renderButtons();
+        return false;
+        });
+    }
 
 
     
     $(document).on("click", ".search-button", displayGifs);
     renderButtons();
+    addNewButton();
     
 //closes page
 });
